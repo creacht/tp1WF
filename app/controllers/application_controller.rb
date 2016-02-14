b2afd21632418e7283528babd6e5dcee0c59edb9
creach_t <theo.creach@gmail.com>
2016-02-14 21:17:01 +0000

    third commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def helloWorld
-    render text: "Hello Kevin"
+    render text: "Hello World"
   end
 end
